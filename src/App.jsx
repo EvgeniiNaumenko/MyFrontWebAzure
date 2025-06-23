97fb01dd26943aa47f910a64ff41251403fe009e
Naumenko Evgenii <evgeniynaumenko2017@gmail.com>
2025-06-23 20:46:28 +0300

    auth

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,60 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [token,setToken] = useState(null);
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState ("");
+
+  const signInClick =() =>{
+    const credentials = btoa(login + ':' + password);
+
+    fetch("https://pv311num6-b9hbdbfsc3gdbfer.canadacentral-01.azurewebsites.net/Cosmos/SignIn", {
+        headers: {
+            'Authorization': 'Basic ' + credentials
+        }
+    }).then(r => r.json())
+        .then(j => {
+            if (j.status && j.status.isOk) {
+                console.log(j.data);
+                setToken(j.data);
+                setName(JSON.parse(atob(j.data.split('.')[1])).nam);
+                // statusBox.textContent = "✅ Entered!";
+                // statusBox.classList.remove("text-danger");
+                // statusBox.classList.add("text-success");
+            } else {
+                // statusBox.textContent = "❌ " + (j.data || "Aut error");
+                // statusBox.classList.remove("text-success");
+                // statusBox.classList.add("text-danger");
+            }
+        })
+        .catch(err => {
+            // statusBox.textContent = "❌ Connection error";
+            // statusBox.classList.remove("text-success");
+            // statusBox.classList.add("text-danger");
+            console.error(err);
+        });
+  }
+
+  const authBlock = () => <> 
+      <span>Login:</span><input value={login} onChange={ e=> setLogin(e.target.value)}></input>
+      <br/>
+      <span>Password:</span><input value={password} onChange={ e=> setPassword(e.target.value)}></input>
+      <br/>
+      <button onClick={signInClick}>Sign In</button>
+     </>;
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {token == null ? authBlock() : 
+        <>
+          <span>Hello, {name} </span>
+          <br />
+          <button onClick={() => setToken(null)}>Sign out</button>
+        </>
+      }
     </>
-  )
+  ); 
 }
 
 export default App
